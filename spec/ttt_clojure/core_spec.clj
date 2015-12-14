5938107fc74302cda187c223fc34fee7b3062abc
bobgu <robertgu84@gmail.com>
2015-12-14 08:50:24 -0800

    can place a piece on the board given a position and piece

--- spec/ttt_clojure/core_spec.clj
+++ spec/ttt_clojure/core_spec.clj
@@ -4,9 +4,10 @@
 
 (describe "empty board"
   (it "creates an empty 3x3 board"
-    (should= [
-        [0 1 2]
-        [3 4 5]
-        [6 7 8]
-      ]
+    (should= [0 1 2 3 4 5 6 7 8]
       empty-board)))
+
+(describe "update-board"
+  (it "returns updated board given piece and position"
+    (should= ["X" 1 2 3 4 5 6 7 8]
+    (update-board [0 1 2 3 4 5 6 7 8] "X" 0))))
--- src/ttt_clojure/core.clj
+++ src/ttt_clojure/core.clj
@@ -1,8 +1,7 @@
 (ns ttt-clojure.core)
 
 (def empty-board
-  [
-    [0 1 2]
-    [3 4 5]
-    [6 7 8]
-  ])
+  [0 1 2 3 4 5 6 7 8])
+
+(defn update-board [board piece position]
+  (assoc board position piece))
