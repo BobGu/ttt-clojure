4c222e11b90a55891f7ce0fa286ee7c055b40792
bobgu <robertgu84@gmail.com>
2015-12-16 10:15:21 -0800

    Rewrite test descriptions so they reflect testing behavior and not implementation

--- spec/ttt_clojure/core_spec.clj
+++ spec/ttt_clojure/core_spec.clj
@@ -24,49 +24,50 @@
   (should (space-available? ["X" "X" 2 3 4 5 "O" "O" 8] 2))))
 
 (describe "game-won?"
-  (it "should return true three of same pieces in top row"
+  (it "should return true if all pieces in the top row are the same"
   (should (game-won? ["X", "X", "X", 3 4 5 6 7 8])))
 
-  (it "should return false if two of same pieces in a row"
-  (should-not (game-won? ["X" "X" 2 3 4 5 6 7 8])))
-
-  (it "should return true if three of same piece in middle row"
+  (it "should return true if all pieces in the middle row are the same"
   (should (game-won? [0 1 2 "X" "X" "X" 6 7 8])))
 
-  (it "should return true if three of same piece in bottom row"
+  (it "should return true if all pieces in bottom row are the same"
   (should (game-won? [0 1 2 3 4 5 "X" "X" "X"])))
 
-  (it "should return true if three of same piece in left column"
-  (should (game-won? ["X" 1 2 "X" 4 5 "X" 7 8]))))
+  (it "should return true if all pieces in left column are the same"
+  (should (game-won? ["X" 1 2 "X" 4 5 "X" 7 8])))
+
+  (it "should return true if all pieces in middle column are the same"
+  (should (game-won? [0 "X" 2 3 "X" 5 6 "X" 8])))
+
+  (it "should return true if all pieces in right column are the same"
+  (should (game-won? [0 1 "X" 3 4 "X" 6 7 "X"]))))
 
 (describe "rows"
-  (it "should return [[0 1 2] [3 4 5] [6 7 8]]"
+  (it "should return the rows given an empty board"
   (should= [[0 1 2] [3 4 5] [6 7 8]] (rows [0 1 2 3 4 5 6 7 8])))
 
-  (it "should return [[\"X\" \"X\" \"X\"] [3 4 5] [6 7 8]"
+  (it "should return correct rows given a board with some spaces taken"
   (should= [["X" "X" "X"] [3 4 5] [6 7 8]] (rows ["X" "X" "X" 3 4 5 6 7 8])))
 
-  (it "should return [[\"O\" 1 2] [3 \"O\" 5] [6 7 8]]"
+  (it "should return correct rows given a board with some spaces taken"
   (should= [["O" 1 2] [3 "O" 5] [6 7 8]] (rows ["O" 1 2 3 "O" 5 6 7 8]))))
 
 (describe "columns"
-  (it "should return [[0 3 6] [1 4 7] [2 5 8]] if board is [0 1 2 3 4 5 6 7 8]"
+  (it "should return the columns given an empty board"
   (should= [[0 3 6] [1 4 7] [2 5 8]] (columns [0 1 2 3 4 5 6 7 8]))))
 
 (describe "possible-wins"
-  (it "should return [[0 1 2] [3 4 5] [6 7 8] [0 3 6] [1 4 7] [2 5 8]]"
+  (it "should return the rows and columns from a board"
   (should= [[0 1 2] [3 4 5] [6 7 8] [0 3 6] [1 4 7] [2 5 8]]
   (possible-wins [0 1 2 3 4 5 6 7 8]))))
 
 (describe "three-in-a-row?"
-  (it "should return true given [\"X\" \"X\" \"X\"]"
-  (should (three-in-a-row? ["X" "X" "X"])))
+  (it "should return true given three of same piece in a row"
+  (should (three-in-a-row? ["X" "X" "X"])
+  (should (three-in-a-row? ["O" "O" "O"]))))
 
-  (it "should return true given [\"O\" \"O\" \"O\"]"
-  (should (three-in-a-row? ["O" "O" "O"])))
-
-  (it "should return false given [\"O\" 1 \"O\"]"
+  (it "should return false given an available space and the rest the same piece"
   (should-not (three-in-a-row? ["O" 1 "O"])))
 
-  (it "should return false given [\"X\" \"O\" \"X\"]"
+  (it "should return false given mostly one piece and one of the opposing piece"
   (should-not (three-in-a-row? ["X" "O" "X"]))))
