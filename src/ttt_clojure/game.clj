4d8da74303d5b1bca6405f3efb8cf09d8875cc3f
bobgu <robertgu84@gmail.com>
2015-12-18 10:55:18 -0800

    game start returns game over if game is won

--- src/ttt_clojure/game.clj
+++ src/ttt_clojure/game.clj
@@ -5,3 +5,7 @@
 (defn game-won? [board]
   (let [possible-wins (possible-wins board)]
   (some true? (map #(all-spaces-the-same? %) possible-wins))))
+
+(defn game-start [board]
+  (if (game-won? board)
+  "Game Over"))
