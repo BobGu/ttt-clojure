bb720893aac0b00e3f3b93830346392163d095c3
Robert Gu <robertgu@Roberts-MacBook-Pro.local>
2015-12-23 09:52:12 -0800

    refactored should see instructions step

--- features/step_definitions/instructions_stepdefs.clj
+++ features/step_definitions/instructions_stepdefs.clj
@@ -2,5 +2,4 @@
 (use 'ttt-clojure.game)
 
 (Then #"^I should see instructions on how to play the game" []
-      (should-contain #"tic tac toe*"
-        (with-in-str "Robert" (with-out-str (start-game)))))
+      (should-contain #"tic tac toe*" (with-out-str (with-in-str "Robert" (start-game)))))
---
+++ features/step_definitions/second_player_name_stepdefs.clj
@@ -0,0 +1,9 @@
+(use 'ttt-clojure.game)
+(use 'speclj.core)
+
+(Given #"^the first player has correctly entered their name and chose a piece" []
+      "the first player has correctly entered their name and chose a piece")
+
+
+
+
